d8bf714ce4ea2650025bbd754a6f97f7304d9a91
saleh <ghalayinisaleh9@gmail.com>
2024-12-05 17:54:06 +0200

    created the function find_peaks that will take a series of numbers converted from a string and will find the peaks in the array trhen return it

---
+++ peaks-in-arrays.js
@@ -0,0 +1,17 @@
+function find_peaks(array) {
+    let peaks = [];
+
+    for (let i = 1; i < array.length - 1; i++) {
+        if (array[i - 1] < array[i] && array[i] > array[i + 1]) {
+            peaks.push(array[i]);
+        }
+    }
+    return peaks;
+}
+
+const input = prompt("Enter the series of numbers with space in between");
+const numbers = input.split(" ").map(Number);
+
+const peaks = find_peaks(numbers);
+
+console.log("Peaks: ", peaks);
